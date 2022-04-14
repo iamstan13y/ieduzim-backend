27209475dc9c58dc55ece1e6b83210529617b902
iamstan13y <iamstan13y@gmail.com>
2022-04-14 11:18:56 +0200

    Exchange Rate Db Migrations.

---
+++ Migrations/AppDb/20220414091223_ExchangeRates.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IEduZimAPI.Migrations.AppDb
+{
+    public partial class ExchangeRates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExchangeRates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CurrencyId = table.Column<int>(type: "int", nullable: false),
+                    Rate = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExchangeRates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExchangeRates_Currencies_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExchangeRates_CurrencyId",
+                table: "ExchangeRates",
+                column: "CurrencyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExchangeRates");
+        }
+    }
+}
---
+++ Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IEduZimAPI.Models.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace IEduZimAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ExchangeRate> ExchangeRates { get; set; }
+    }
+}
