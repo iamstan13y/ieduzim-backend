e38831a3c0c6202f4b834974eea8ac1bd59cb732
iamstan13y <iamstan13y@gmail.com>
2022-04-16 23:07:15 +0200

    Locations Controller.

---
+++ Controllers/LocationsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IEduZimAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+    }
+}
