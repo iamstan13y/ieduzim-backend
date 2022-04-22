bcefe53e5ed8f68c6b3868149393db927221a222
iamstan13y <iamstan13y@gmail.com>
2022-04-22 10:48:04 +0200

    Docker containerization.

--- Startup.cs
+++ Startup.cs
@@ -139,7 +139,7 @@ namespace IEduZimAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
