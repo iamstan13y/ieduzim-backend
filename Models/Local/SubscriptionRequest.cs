adc8a1489e252b843c303b99585c8ef89d902cc1
iamstan13y <iamstan13y@gmail.com>
2022-04-28 10:03:38 +0200

    Amount data type.

--- Models/Local/SubscriptionRequest.cs
+++ Models/Local/SubscriptionRequest.cs
@@ -6,7 +6,7 @@ namespace IEduZimAPI.Models.Local
     {
         public string UserId { get; set; }
         public int SubjectId { get; set; }
-        public int Amount { get; set; }
+        public double Amount { get; set; }
         public string PhoneNumber { get; set; }
         public PaymentMethod PaymentMethod { get; set; }
         public int PaymentPeriod { get; set; }
