82dca21892c002c9de075e2d5f4687e6067eb24b
iamstan13y <iamstan13y@gmail.com>
2022-05-08 22:32:30 +0200

    Subject Model DB Migrations.

--- Migrations/AppDb/20220508203143_SubjectTeacherIdDetach.Designer.cs
+++ Migrations/AppDb/20220508203143_SubjectTeacherIdDetach.Designer.cs
@@ -4,14 +4,16 @@ using IEduZimAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IEduZimAPI.Migrations.AppDb
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220508203143_SubjectTeacherIdDetach")]
+    partial class SubjectTeacherIdDetach
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/AppDb/20220508203143_SubjectTeacherIdDetach.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IEduZimAPI.Migrations.AppDb
+{
+    public partial class SubjectTeacherIdDetach : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subjects_Teachers_TeacherId",
+                table: "Subjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subjects_TeacherId",
+                table: "Subjects");
+
+            migrationBuilder.DropColumn(
+                name: "TeacherId",
+                table: "Subjects");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TeacherId",
+                table: "Subjects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_TeacherId",
+                table: "Subjects",
+                column: "TeacherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subjects_Teachers_TeacherId",
+                table: "Subjects",
+                column: "TeacherId",
+                principalTable: "Teachers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
