84dce5152160c87aefc5773dc2948548fed41f6a
iamstan13y <iamstan13y@gmail.com>
2022-05-11 19:58:05 +0200

    Lesson Status Migrations.

---
+++ Migrations/AppDb/20220511175534_LessonStatus.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IEduZimAPI.Migrations.AppDb
+{
+    public partial class LessonStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Confirmed",
+                table: "Lessons");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LessonStatus",
+                table: "Lessons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LessonStatus",
+                table: "Lessons");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Confirmed",
+                table: "Lessons",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
