803ff4d117c962529ab9824ec4fe52d6da59a0f0
iamstan13y <iamstan13y@gmail.com>
2022-06-16 09:52:54 +0200

    Lesson schedule date time format - II.

--- Models/Local/LessonScheduleRequest.cs
+++ Models/Local/LessonScheduleRequest.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace IEduZimAPI.Models.Local
 {
     public class LessonScheduleRequest
     {
         public int StudentId { get; set; }
-        //public int SubjectId { get; set; }
         public int LessonStructureId { get; set; }
         public List<int> LessonDays { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
     }
 }
