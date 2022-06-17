7f6fcb01a2532f8089432e76ac89f3f4fc54a6fc
iamstan13y <iamstan13y@gmail.com>
2022-06-17 23:52:37 +0200

    Hybrid Schedule - IV.

---
+++ Models/Data/HybridLessonSchedule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IEduZimAPI.Models.Data
+{
+    public class HybridLessonSchedule
+    {
+        public int Id { get; set; }
+        public int LessonDay { get; set; }
+        public int SubjectId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
