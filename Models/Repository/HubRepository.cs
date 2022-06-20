fde44c6dc9e1f8058cc5218783d24d208ab04ee0
iamstan13y <iamstan13y@gmail.com>
2022-06-20 22:35:06 +0200

    Hub Repository Pattern & DI.

---
+++ Models/Repository/HubRepository.cs
@@ -0,0 +1,34 @@
+﻿using IEduZimAPI.Models.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IEduZimAPI.Models.Repository
+{
+    public class HubRepository : IHubRepository
+    {
+        public Task<Hub> AddAsync(Hub hub)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Hub> DeleteAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Hub>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Hub> GetByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Hub> UpdateAsync(Hub hub)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
