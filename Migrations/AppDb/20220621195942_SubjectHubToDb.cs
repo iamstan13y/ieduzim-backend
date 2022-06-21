be53737c0af2c5deb540419e2b9149094116460b
iamstan13y <iamstan13y@gmail.com>
2022-06-21 22:09:00 +0200

    Subject->Hub rltnshp.

---
+++ Migrations/AppDb/20220621195942_SubjectHubToDb.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IEduZimAPI.Migrations.AppDb
+{
+    public partial class SubjectHubToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HubId",
+                table: "Subjects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HubId",
+                table: "Subjects");
+        }
+    }
+}
