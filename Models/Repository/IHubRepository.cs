378cc52bd7851d25dfc5efcf38ab60280608bebc
iamstan13y <iamstan13y@gmail.com>
2022-06-21 03:36:34 +0200

    Hub Repository Pattern & DI.

---
+++ Models/Repository/IHubRepository.cs
@@ -0,0 +1,15 @@
+﻿using IEduZimAPI.Models.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IEduZimAPI.Models.Repository
+{
+    public interface IHubRepository
+    {
+        Task<IEnumerable<Hub>> GetAllAsync();
+        Task<Hub> AddAsync(Hub hub);
+        Task<Hub> GetByIdAsync(int id);
+        Task<Hub> UpdateAsync(Hub hub);
+        Task<Hub> DeleteAsync(int id);
+    }
+}
