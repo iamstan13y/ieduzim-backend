796dc9273563f6dc8d34ab2be13af458a736ccc8
iamstan13y <iamstan13y@gmail.com>
2022-06-21 04:16:53 +0200

    Update Hub action method.

---
+++ Models/Local/UpdateHubRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace IEduZimAPI.Models.Local
+{
+    public class UpdateHubRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int LocationId { get; set; }
+    }
+}
