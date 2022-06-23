d4e3ec054964379e62c988859af675bae3391d70
iamstan13y <iamstan13y@gmail.com>
2022-06-23 23:17:05 +0200

    Add Subject to Hub - I.

---
+++ Migrations/AppDb/20220623204532_SubjectHubToDbII.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IEduZimAPI.Migrations.AppDb
+{
+    public partial class SubjectHubToDbII : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HubId",
+                table: "Subjects");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HubId",
+                table: "Subjects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
