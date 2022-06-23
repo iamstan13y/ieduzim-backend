f54913213b1e76a4890045a1eeeaaa4755e43689
iamstan13y <iamstan13y@gmail.com>
2022-06-23 23:17:43 +0200

    Add Subject to Hub - I.

---
+++ Models/Data/HubLessonSchedule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IEduZimAPI.Models.Data
+{
+    public class HubLessonSchedule
+    {
+        public int Id { get; set; }
+        public int LessonDay { get; set; }
+        public int SubjectId { get; set; }
+        public int HubId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
