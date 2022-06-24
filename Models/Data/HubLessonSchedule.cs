14a09784ebc172e31e2714de8794cab755e10f80
iamstan13y <iamstan13y@gmail.com>
2022-06-24 23:52:13 +0200

    Hub Response fixes - I.

--- Models/Data/HubLessonSchedule.cs
+++ Models/Data/HubLessonSchedule.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using IEduZimAPI.Models.Enums;
+using System;
 
 namespace IEduZimAPI.Models.Data
 {
     public class HubLessonSchedule
     {
         public int Id { get; set; }
-        public int LessonDay { get; set; }
+        public Day LessonDay { get; set; }
         public int SubjectId { get; set; }
         public int HubId { get; set; }
         public DateTime StartTime { get; set; }
