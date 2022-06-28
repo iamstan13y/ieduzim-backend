04df3347aeab14d4b0c32885cf90684142dd5135
iamstan13y <iamstan13y@gmail.com>
2022-06-28 16:22:53 +0200

    Subscription Lesson Schedules - I.

---
+++ Models/Data/StudentLessonSchedule.cs
@@ -0,0 +1,9 @@
+﻿namespace IEduZimAPI.Models.Data
+{
+    public class StudentLessonSchedule
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int LessonScheduleId { get; set; }
+    }
+}
