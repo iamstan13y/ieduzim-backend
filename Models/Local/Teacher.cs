568ce7af92ab12ab266757f1050adda19b6093f9
iamstan13y <iamstan13y@gmail.com>
2022-08-05 23:49:23 +0200

    Modified Teacher model - I.

--- Models/Local/Teacher.cs
+++ Models/Local/Teacher.cs
@@ -4,16 +4,20 @@ namespace IEduZimAPI.Models.Local
 {
     public class Teachers
     {
-        [NotMapped]
         public string PhoneNumber { get; set; }
         public int TitleId { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Gender { get; set; }
-        public string QualificationDescription { get; set; }
-        public string ProfilePicture { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string EducationalQualification { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public string Occupation { get; set; }
+        public string City { get; set; }
         public string UserId { get; set; }
         public bool Verified { get; set; }
         public bool Subscribed { get; set; }
     }
 }
+//Teachers
+//6.Bank Account details
