5a580852ffc3f61fceb6485d2f68eba58957fa66
iamstan13y <iamstan13y@gmail.com>
2022-08-06 06:37:08 +0200

    Add Teacher Implementation.

--- Models/Repository/TeacherRepository.cs
+++ Models/Repository/TeacherRepository.cs
@@ -1,14 +1,33 @@
 ﻿using IEduZimAPI.CoreClasses;
 using IEduZimAPI.Models.Data;
+using System;
 using System.Threading.Tasks;
 
 namespace IEduZimAPI.Models.Repository
 {
     public class TeacherRepository : ITeacherRepository
     {
-        public Task<Result<Teacher>> AddAsync(Teacher teacher)
+        private readonly AppDbContext _context;
+
+        public TeacherRepository(AppDbContext context)
         {
-            throw new System.NotImplementedException();
+            _context = context;
+        }
+
+        public async Task<Result<Teacher>> AddAsync(Teacher teacher)
+        {
+            try
+            {
+                await _context.Teachers!.AddAsync(teacher);
+
+                await _context.SaveChangesAsync();
+
+                return new Result<Teacher>(teacher);
+            }
+            catch (Exception)
+            {
+                return new Result<Teacher>(false, "Failed to add Teacher.", null);
+            }
         }
     }
 }
