89ca39d0f67e204529c938e041d17ffda15fbe38
iamstan13y <iamstan13y@gmail.com>
2022-08-06 13:08:28 +0200

    File service config.

---
+++ Models/Repository/FileRepository.cs
@@ -0,0 +1,28 @@
+﻿using IEduZimAPI.CoreClasses;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IEduZimAPI.Models.Repository
+{
+    public class FileRepository : IFileRepository
+    {
+        private readonly AppDbContext _context;
+
+        public FileRepository(AppDbContext context) => _context = context;
+      
+        public async Task<Result<UploadFile>> AddAsync(UploadFile imageFile)
+        {
+            await _context.UploadFiles!.AddAsync(imageFile);
+            await _context.SaveChangesAsync();
+
+            return new Result<UploadFile>(imageFile);
+        }
+
+        public async Task<Result<IEnumerable<UploadFile>>> GetAllAsync()
+        {
+            var files = await _context.UploadFiles!.ToListAsync();
+            return new Result<IEnumerable<UploadFile>>(files);
+        }
+    }
+}
