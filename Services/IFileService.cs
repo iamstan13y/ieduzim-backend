d34e1769bc663a32b33e2ac9de309e7c4980ede9
iamstan13y <iamstan13y@gmail.com>
2022-08-06 13:07:55 +0200

    File service config.

---
+++ Services/IFileService.cs
@@ -0,0 +1,12 @@
+﻿using IEduZimAPI.CoreClasses;
+using IEduZimAPI.Models;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace IEduZimAPI.Services
+{
+    public interface IFileService
+    {
+        Task<Result<UploadFile>> UploadFileAsync(IFormFile formFile);
+    }
+}
