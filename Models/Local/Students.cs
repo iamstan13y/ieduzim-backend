c9db0d7ea53a926c7ca27c48f68ffca91ef7d4cf
iamstan13y <iamstan13y@gmail.com>
2022-08-06 07:12:34 +0200

    Student additions & DB Migrations.

--- Models/Local/Students.cs
+++ Models/Local/Students.cs
@@ -7,6 +7,13 @@
         public string Surname { get; set; }
         public int TitleId { get; set; }
         public string Gender { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AcademicLevel { get; set; }
+        public string EnrolledSchool { get; set; }
+        public string City { get; set; }
+        public string NextOfKin { get; set; }
+        public string NextOfKinContact { get; set; }
         public int LocationId { get; set; } 
     }
 }
