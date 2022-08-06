ae2cf596cf26874b8faab721b1188ac72812d768
iamstan13y <iamstan13y@gmail.com>
2022-08-06 06:35:31 +0200

    Teacher Addition & DB migratons.

---
+++ Migrations/AppDb/20220806043441_TeacherAdditionsIIToDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IEduZimAPI.Migrations.AppDb
+{
+    public partial class TeacherAdditionsIIToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BankAccount",
+                table: "Teachers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "QualificationUrl",
+                table: "Teachers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BankAccount",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "QualificationUrl",
+                table: "Teachers");
+        }
+    }
+}
