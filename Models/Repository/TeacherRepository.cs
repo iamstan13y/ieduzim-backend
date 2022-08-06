d0762c779aa124a4c2543d2962ea7c15ecb53b0b
iamstan13y <iamstan13y@gmail.com>
2022-08-06 05:42:18 +0200

    Teacher Repository, interface & DI.

---
+++ Models/Repository/TeacherRepository.cs
@@ -0,0 +1,14 @@
+﻿using IEduZimAPI.CoreClasses;
+using IEduZimAPI.Models.Data;
+using System.Threading.Tasks;
+
+namespace IEduZimAPI.Models.Repository
+{
+    public class TeacherRepository : ITeacherRepository
+    {
+        public Task<Result<Teacher>> AddAsync(Teacher teacher)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
