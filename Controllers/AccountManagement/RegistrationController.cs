348ebe503956a75805a1306065d98acb5fb9b266
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 02:51:02 +0200

    Activate Studenrt Account.

--- Controllers/AccountManagement/RegistrationController.cs
+++ Controllers/AccountManagement/RegistrationController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Identity;
 using IEduZimAPI.CoreClasses;
 using IEduZimAPI.Models.Data;
+using Microsoft.AspNetCore.Authorization;
+using IEduZimAPI.Models;
 
 namespace IEduZimAPI.Services.AccountServices
 {
@@ -10,16 +12,22 @@ namespace IEduZimAPI.Services.AccountServices
     public class RegistrationController : ControllerBase
     {
         RegistrationService service;
-        public RegistrationController(UserManager<IdentityUser> manager, RoleManager<IdentityRole> roleManager) =>
-            service = new RegistrationService(manager, roleManager);
+        public RegistrationController(UserManager<IdentityUser> manager, RoleManager<IdentityRole> roleManager, AppDbContext context) =>
+            service = new RegistrationService(manager, roleManager, context);
 
         [HttpPost]
         public Result<bool> Register([FromBody] Register register) =>
             ExecutionService.Execute(() => service.Register(register), "Account Creation Successful. Check your email for activation link.");
 
         [HttpPost]
-        [Route("activate")]
-        public Result<bool> ActivateAccount([FromBody] EmailConfirmation confirmation) =>
-            ExecutionService.Execute(() => service.ActivateAccount(confirmation), "Account activated.");
+        [Route("activate/student/{studentId}")]
+        public Result<bool> ActivateStudent(int studentId) =>
+            ExecutionService.Execute(() => service.ActivateStudent(studentId), "Account activated.");
+
+        [HttpPost]
+        [Authorize]
+        [Route("activate/teacher")]
+        public Result<bool> ActivateTeacher(string teacherId) =>
+            ExecutionService.Execute(() => service.ActivateTeacher(teacherId), "Account activated.");
     }
 }
