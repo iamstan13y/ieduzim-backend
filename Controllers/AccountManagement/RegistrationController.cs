f452cbc6c6f4b782e05b0bae97058876511e8d70
Keith Stanley <iamstan13y@gmail.com>
2022-10-10 22:10:36 +0200

    Code Cleanup - I.

--- Controllers/AccountManagement/RegistrationController.cs
+++ Controllers/AccountManagement/RegistrationController.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Identity;
-using IEduZimAPI.CoreClasses;
-using IEduZimAPI.Models.Data;
-using Microsoft.AspNetCore.Authorization;
+﻿using IEduZimAPI.CoreClasses;
 using IEduZimAPI.Models;
+using IEduZimAPI.Models.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IEduZimAPI.Services.AccountServices
 {
@@ -18,7 +17,7 @@ namespace IEduZimAPI.Services.AccountServices
         [HttpPost]
         public Result<IdentityUser> Register([FromBody] Register register) =>
             service.Register(register);
-        
+
         [HttpPost]
         [Route("activate/teacher/{teacherId}")]
         public Result<bool> ActivateTeacher(int teacherId) =>
