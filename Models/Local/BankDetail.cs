ff1cbcff743ce42df58da351976ad9e37bc07ff2
Keith Stanley <iamstan13y@gmail.com>
2022-10-13 19:24:07 +0200

    Minor refactors - II.

--- Models/Local/BankDetail.cs
+++ Models/Local/BankDetail.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace IEduZimAPI.Models.Local
+﻿namespace IEduZimAPI.Models.Local
 {
     public class BankDetail
     {
