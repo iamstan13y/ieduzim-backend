452147f72cc347da1fe07f68e859a5ffe2d0a8e0
Keith Stanley <iamstan13y@gmail.com>
2022-10-14 19:21:57 +0200

    Minor refactors - II.

--- Models/Data/Subject.cs
+++ Models/Data/Subject.cs
@@ -1,9 +1,9 @@
 ﻿namespace IEduZimAPI.Models.Data
 {
-    public class Subject:Local.Subjects
+    public class Subject : Local.Subjects
     {
-       public int Id { get; set; }
-       public virtual Currencies Currency { get; set; }
-       public virtual Level Level { get; set; }
+        public int Id { get; set; }
+        public virtual Currencies Currency { get; set; }
+        public virtual Level Level { get; set; }
     }
 }
